fff511023268dce417d7ac950907d6465c34cd35
zorkz <alexandre.stachnik2@gmail.com>
2024-03-05 18:36:38 +0100

    MERCI MAN

--- srcs/wordpress/tools/wordpress_start.sh
+++ srcs/wordpress/tools/wordpress_start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+	sed -i "s/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/" "/etc/php/8.2/fpm/pool.d/www.conf";
 	chown -R www-data:www-data /var/www/*;
-	chown -R 755 /var/www/*;
+	chown -R 777 /var/www/*;
 	mkdir -p /run/php/;
-	touch /run/php/php7.3-fpm.pid;
+	touch /run/php/php8.2-fpm.pid;
+	rm -rf /var/www/html/*
+
 
 if [ ! -f /var/www/html/wp-config.php ]; then
 	echo "Wordpress: setting up..."
@@ -14,7 +16,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	mv wp-cli.phar /usr/local/bin/wp;
 	cd /var/www/html;
 	wp core download --allow-root;
-	mv /var/www/wp-config.php /var/www/html/
+	cp /var/www/wp-config.php /var/www/html/
 	# echo "Wordpress: creating users..."
 	# wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	# wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
