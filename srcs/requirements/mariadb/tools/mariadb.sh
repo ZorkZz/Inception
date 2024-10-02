aa41d878fff6cd2f4064520d5d490fc4b5ecf967
zorkz <alexandre.stachnik2@gmail.com>
2024-10-02 17:54:14 +0200

    mariadb

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -5,3 +5,15 @@ service mariadb start
 while ! mysqladmin ping -h"localhost" --silent; do
     sleep 1
 done
+
+
+mysql -u root -p << EOF
+CREATE DATABASE IF NOT EXISTS $DB_NAME;
+CREATE USER IF NOT EXISTS '$USER_NAME'@'%' IDENTIFIED BY '$USER_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$USER_NAME'@'%';
+FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'zizi';
+EOF
+
+
+exec "$@"
